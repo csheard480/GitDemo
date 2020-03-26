6ccca040aa14d0fff9f2005335b01b37124e3baf
csheard480 <60625964+csheard480@users.noreply.github.com>
2020-03-26 15:55:02 -0400

    Add files via upload

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+//160: Integration of Testng with Maven
+	//TestNG xml file from Maven
+	//How to execute all test cases from Test folder - mvn test 
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+}
